a2bfea55dfa36478e370243e20e70b32ce98796f
root <jahbini@jahbini.org>
2018-06-07 09:10:39 +0000

    regain old bamboo snow concrete5 php site

---
+++ index.php/tools/blocks/wordpress_entries/rss52cb.php
@@ -0,0 +1,17 @@
+<?php         xml version="1.0"?>
+<rss version="2.0">
+	<channel>
+		<title>Blog Feed</title>
+		<link>http://bamboo-snow.com/index.php/tools/blocks/wordpress_entries/rss.php?bID=72&amp;cID=146</link>
+		<description></description>
+		<copyright>Copyright 2018</copyright>
+
+		<item>
+			<title>Why You Have Never Heard of Bamboo Snow</title>
+			<link>http://bamboo-snow.com/bamboo-snow/faq/?p=18758</link>
+			<description>&lt;p&gt;A long time ago, before there were facts, there were two brothers Adolfo and Alejandro.  That is Brother Adolfo and Alejandro were in fact brothers-in-law.  Adolfo’s sister Maria was married to the Alcalde of Saipan: Alejandro. At that time, Saipan &amp;hellip; &lt;a href=&quot;http://bamboocando.com/why-you-have-never-heard-of-bamboo-snow/&quot;&gt;Continue reading &lt;span class=&quot;meta-nav&quot;&gt;&amp;rarr;&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+</description>
+			<pubDate>Fri, 01 Nov 2013 00:39:18 PDT</pubDate>
+		</item>
+	</channel>
+</rss>
